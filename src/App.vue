cbaea17e9c1b0b4fd2fb9c132f0c101a53b0e76c
Feres Henteti <fereshenteti@gmail.com>
2025-02-03 23:11:14 +0100

    added meta to prevent dark mode filters on some browsers

--- src/App.vue
+++ src/App.vue
@@ -201,6 +201,8 @@ onMounted(() => {
             <h2>Feres</h2>
             <h2>& Nermine</h2>
             <h1>👰‍♀️🤵🏻</h1>
+            <h2 class="save-the-date">Save the</h2>
+            <h2 class="save-the-date">date <span class="ring">💍</span></h2>
           </div>
         </div>
         <div class="page back">
